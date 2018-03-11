da74c77b9894ea3816b1e8ec8b34773abbbe8484
pirocorp <pirocorp@abv.bg>
2018-03-11 13:09:39 +0100

    12. Multiple Values for a Key

---
+++ 09. JavaScript Syntax, Basic Web (DOM, Events) - Exercise/12. Multiple Values for a Key.js
@@ -0,0 +1,25 @@
+function solve(inputLines) {
+    let arr = [];
+    for (let i = 0; i < inputLines.length - 1; i++) {
+        let tokens = inputLines[i].split(' ');
+        let key = tokens[0];
+        let value = tokens[1];
+
+        if (key in arr) {
+            arr[key].push(value);
+        } else {
+            arr[key] = [];
+            arr[key].push(value);
+        }
+    }
+
+    let key = inputLines[inputLines.length - 1];
+
+    if(key in arr){
+        console.log(arr[key].join("\n"));
+    } else {
+        console.log('None')
+    }
+}
+
+solve(['key value','key eulav','test tset','3']);
