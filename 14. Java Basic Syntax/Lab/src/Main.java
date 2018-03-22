5dfca0f91b128a4850922920a7e58e25f81326f8
pirocorp <pirocorp@abv.bg>
2018-03-22 21:17:02 +0100

    Java Basic Syntax

---
+++ 14. Java Basic Syntax/Lab/src/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+
+        double val = (30 + 21) * 1.00 / 2.00 * (35 - 12 - 1.00 / 2.00);
+        double result = Math.pow(val, 2);
+        System.out.println(result);
+    }
+}
---
+++ 14. Java Basic Syntax/Lab/src/ThreeIntegersSum.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ThreeIntegersSum {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String inputLine = scan.nextLine();
+        int[] arr = Arrays
+                .stream(inputLine.split(" "))
+                .mapToInt(x -> Integer.valueOf(x))
+                .sorted()
+                .toArray();
+
+        int num1 = Array.getInt(arr, 0);
+        int num2 = Array.getInt(arr, 1);
+        int num3 = Array.getInt(arr, 2);
+
+        if(num1 + num2 == num3) {
+            System.out.println(num1 + " + " + num2 + " = " + num3);
+        } else if (num1 == num2 + num3){
+            System.out.println(num2 + " + " + num3 + " = " + num1);
+        } else if(num2 == num1 + num3){
+            System.out.println(num1 + " + " + num3 + " = " + num2);
+        } else {
+            System.out.println("No");
+        }
+
+        //System.out.println(Arrays.toString(arr)); //-> print array
+    }
+}
