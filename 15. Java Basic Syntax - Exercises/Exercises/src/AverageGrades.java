9e24240ef2b7fb4b21163063db1c908602f3300e
pirocorp <pirocorp@abv.bg>
2018-03-25 00:14:25 +0100

    23. Average Grades -> some refact

--- 15. Java Basic Syntax - Exercises/Exercises/src/AverageGrades.java
+++ 15. Java Basic Syntax - Exercises/Exercises/src/AverageGrades.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
-import static java.util.Collections.reverseOrder;
 import static java.util.Comparator.comparing;
 
 public class AverageGrades {
