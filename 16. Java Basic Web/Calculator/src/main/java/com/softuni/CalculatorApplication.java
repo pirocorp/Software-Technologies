6b9d47bca4b0e32ca9d816da0535f396d47c655c
pirocorp <pirocorp@abv.bg>
2018-03-26 18:39:55 +0200

    Calculator

--- 16. Java Basic Web/Calculator/src/main/java/com/softuni/CalculatorApplication.java
+++ 16. Java Basic Web/Calculator/src/main/java/com/softuni/CalculatorApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
+
 	}
 }
--- 16. Java Basic Web/Calculator/src/main/java/com/softuni/controller/HomeController.java
+++ 16. Java Basic Web/Calculator/src/main/java/com/softuni/controller/HomeController.java
@@ -1,14 +1,54 @@
 package com.softuni.controller;
 
+import com.softuni.entity.Calculator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("operator", "+");
+		model.addAttribute("view", "views/calculatorForm");
 		return "base-layout";
 	}
+
+    @PostMapping("/")
+    public String calculate(@RequestParam String leftOperand,
+                            @RequestParam String operator,
+                            @RequestParam String rightOperand,
+                            Model model){
+
+        double num1 = parseNum(leftOperand);
+        double num2 = parseNum(rightOperand);
+
+        Calculator calculator = new Calculator(num1, num2, operator);
+
+        double result = calculator.calculateResult();
+
+        model.addAttribute("leftOperand", calculator.getLeftOperand());
+        model.addAttribute("operator", calculator.getOperator());
+        model.addAttribute("rightOperand", calculator.getRightOperand());
+
+        model.addAttribute("result", result);
+
+        model.addAttribute("view","views/calculatorForm");
+
+        return "base-layout";
+    }
+
+    private double parseNum(String num) {
+
+        double number;
+        try {
+            number = Double.parseDouble(num);
+        } catch (NumberFormatException ex) {
+            number = 0;
+        }
+
+        return number;
+    }
 }
