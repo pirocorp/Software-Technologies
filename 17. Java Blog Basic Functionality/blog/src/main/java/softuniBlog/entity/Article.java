ad20a86e67adfbbcd2034a6150a5e18e1fea5337
pirocorp <pirocorp@abv.bg>
2018-03-27 21:43:15 +0200

    Java Blog basic functionality with Hibernate ORM, Spring Framework and Thymeleaf template engine

---
+++ 17. Java Blog Basic Functionality/blog/src/main/java/softuniBlog/entity/Article.java
@@ -0,0 +1,81 @@
+package softuniBlog.entity;
+
+import javax.persistence.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Entity
+@Table(name = "articles")
+public class Article {
+
+    private Integer id;
+
+    private String title;
+
+    private String content;
+
+    private User author;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(nullable = false)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(columnDefinition = "text", nullable = false)
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "authorId")
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Article(String title, String content, User author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Article() {    }
+
+    @Transient
+    public String getSummary(){
+        Pattern pattern = Pattern.compile(".{50,}?\\s");
+        String content = this.getContent();
+        Matcher matcher = pattern.matcher(content);
+        String summary;
+
+        if(content.length() > 100 && matcher.find()){
+            summary = matcher.group(0);
+            System.out.println("Regex matching");
+        } else {
+            summary = content.substring(0, content.length() / 2);
+        }
+
+        return summary;
+    }
+}
