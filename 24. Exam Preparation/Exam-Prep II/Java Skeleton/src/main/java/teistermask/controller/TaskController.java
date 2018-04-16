aa8a501a7ac4fd6db8d89bec6593e998b2f75aea
pirocorp <pirocorp@abv.bg>
2018-04-16 20:21:55 +0200

    Java prep II

--- 24. Exam Preparation/Exam-Prep II/Java Skeleton/src/main/java/teistermask/controller/TaskController.java
+++ 24. Exam Preparation/Exam-Prep II/Java Skeleton/src/main/java/teistermask/controller/TaskController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import teistermask.bindingModel.TaskBindingModel;
+import teistermask.entity.Task;
 import teistermask.repository.TaskRepository;
+
+import java.util.List;
 
 @Controller
 public class TaskController {
@@ -20,26 +23,67 @@ public class TaskController {
 
 	@GetMapping("/")
 	public String index(Model model) {
-		// TODO: Implement me...
+		List<Task> openTasks = taskRepository.findAllByStatus("Open");
+		List<Task> inProgressTasks = taskRepository.findAllByStatus("In Progress");
+		List<Task> finishedTasks = taskRepository.findAllByStatus("Finished");
+
+		model.addAttribute("openTasks", openTasks);
+		model.addAttribute("inProgressTasks", inProgressTasks);
+		model.addAttribute("finishedTasks", finishedTasks);
+		model.addAttribute("view", "task/index");
+
+		return "base-layout";
 	}
 
 	@GetMapping("/create")
 	public String create(Model model) {
-		// TODO: Implement me...
+		model.addAttribute("view", "task/create");
+		return "base-layout";
 	}
 
 	@PostMapping("/create")
 	public String createProcess(Model model, TaskBindingModel taskBindingModel) {
-		// TODO: Implement me...
+		if(taskBindingModel == null){
+			return "redirect:/";
+		}
+
+		if(taskBindingModel.getTitle().equals("") || taskBindingModel.getStatus().equals("")){
+			return "redirect:/";
+		}
+
+		Task task = new Task(taskBindingModel.getTitle(), taskBindingModel.getStatus());
+		taskRepository.saveAndFlush(task);
+
+		return "redirect:/";
 	}
 
 	@GetMapping("/edit/{id}")
 	public String edit(Model model, @PathVariable int id) {
-		// TODO: Implement me...
+		Task task = taskRepository.findOne(id);
+
+		if(task == null){
+			return "redirect:/";
+		}
+
+		model.addAttribute("view", "task/edit");
+		model.addAttribute("task", task);
+
+		return "base-layout";
 	}
 
 	@PostMapping("/edit/{id}")
 	public String editProcess(@PathVariable int id, TaskBindingModel taskBindingModel) {
-		// TODO: Implement me...
+		Task task = taskRepository.findOne(id);
+
+		if(task == null){
+			return "redirect:/";
+		}
+
+		task.setTitle(taskBindingModel.getTitle());
+		task.setStatus(taskBindingModel.getStatus());
+
+		this.taskRepository.saveAndFlush(task);
+
+		return "redirect:/";
 	}
 }
--- 24. Exam Preparation/Exam-Prep II/Java Skeleton/src/main/java/teistermask/entity/Task.java
+++ 24. Exam Preparation/Exam-Prep II/Java Skeleton/src/main/java/teistermask/entity/Task.java
@@ -5,5 +5,41 @@ import javax.persistence.*;
 @Entity
 @Table(name = "tasks")
 public class Task {
-	// TODO: Implement me...
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String status;
+
+    public Task() {
+    }
+
+    public Task(String title, String status) {
+        this.title = title;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
